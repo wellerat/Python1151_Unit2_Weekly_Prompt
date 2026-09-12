3f0e0a15045c47c7c41ba7b66120e06356573c16
Ann Cooper <wellerat@yahoo.com>
2026-09-11 21:41:30 -0400

    Adding the file back in

---
+++ Unit_2_Weekly_Prompt.py
@@ -0,0 +1,19 @@
+# Unit 2: Weekly Prompt  - Ann Cooper
+
+# When creating a dicitonary, keys are used to reference a value.
+# What are the rules for creating 'keys'?
+# Are dictionaries ordered?
+
+# When using a dictionary in Python, there are a few rules about keys - a key
+# can be a number, a string, a Boolean or tuple.  Each key should be unique, but if 
+# there are duplicates, the last key in the list is the one used.  Each key receives
+# a value, and the values do not ahve to be unique.  Keys must be immutable, which 
+# means the key must be a data type hat cannon thange after it was created.
+
+# Dictionaires are orderes - they remain in the order they were created.  If you change
+# the value of a key-value pair, the order of the dictionary remains the same.  If the
+# key-value pair was deleted from the dictionary completely and then re-added, the new 
+# position of the key-value pair woudl be at the end of the list.  If a list needs to be
+# sortedin a different way, there are some tools, such as using the sorted() function
+# with a for loop.  This will change the output of the inforamtion to be sorted
+# but it will not change the order of the dictionary.
